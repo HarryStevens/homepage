beaec3c85121911961d8b95acd0174b34a7c1d99
HarryStevens <harryjosephstevens@gmail.com>
2022-06-09 14:44:12 -0400

    Spaces

--- build/scrape_blocks.js
+++ build/scrape_blocks.js
@@ -1,5 +1,5 @@
 const fs = require("fs"),
-    	scraperama = require("scraperama");
+      scraperama = require("scraperama");
 
 // empty json
 const json = [];
@@ -10,56 +10,56 @@ const scrape_gist_limited = scraperama.throttle(scrape_gist, 5000);
 // find out how many gists there are
 scraperama.html("https://gist.github.com/harrystevens", ($) => {
 
-	console.log("Data will output as data/blocks.json");
+  console.log("Data will output as data/blocks.json");
 
-	const number = Math.ceil($("#gist-pjax-container > div > div > div.col-9.col-md-9.col-12 > div.pagehead.py-0.border-0.mt-md-0.mt-6.mr-md-0.mr-n3.ml-md-0.ml-n3 > nav > div.UnderlineNav-body > a.js-selected-navigation-item.selected.UnderlineNav-item > span").text() / 10);
-	console.log(`Total gist pages: ${number}`)
+  const number = Math.ceil($("#gist-pjax-container > div > div > div.col-9.col-md-9.col-12 > div.pagehead.py-0.border-0.mt-md-0.mt-6.mr-md-0.mr-n3.ml-md-0.ml-n3 > nav > div.UnderlineNav-body > a.js-selected-navigation-item.selected.UnderlineNav-item > span").text() / 10);
+  console.log(`Total gist pages: ${number}`)
 
-	for (let i = 1; i <= number; i++){
-		scrape_gist_limited(i);
-	}
+  for (let i = 1; i <= number; i++){
+    scrape_gist_limited(i);
+  }
 
 });
-	
+  
 function scrape_gist(n){
 
-	scraperama.html(`https://gist.github.com/harrystevens?page=${n}`, ($) => {
+  scraperama.html(`https://gist.github.com/harrystevens?page=${n}`, ($) => {
 
-		$(".gist-snippet").each(function(block_index, block){
-			
-			// make sure the gist is a block
-			if ($(block).find("a:nth-of-type(2)").text() == ".block"){
-			
-				// an empty object for the block info
-				const obj = {};
+    $(".gist-snippet").each(function(block_index, block){
+      
+      // make sure the gist is a block
+      if ($(block).find("a:nth-of-type(2)").text() == ".block"){
+      
+        // an empty object for the block info
+        const obj = {};
 
-				obj.block_number = (n - 1) * 10 + (block_index + 1);
+        obj.block_number = (n - 1) * 10 + (block_index + 1);
 
-				obj.category = "Blocks";
-				obj.headline = $(block).find("span.f6.color-fg-muted").text().trim();
-				obj.date = $(block).find("time-ago").text().trim();
-				obj.publication = "bl.ocks.org";
-				obj.slug = slugify(obj.headline);
-				const url_end = $(block).find("a:nth-of-type(2)").attr("href");
-				obj.url = `https://bl.ocks.org${url_end}`;
+        obj.category = "Blocks";
+        obj.headline = $(block).find("span.f6.color-fg-muted").text().trim();
+        obj.date = $(block).find("time-ago").text().trim();
+        obj.publication = "bl.ocks.org";
+        obj.slug = slugify(obj.headline);
+        const url_end = $(block).find("a:nth-of-type(2)").attr("href");
+        obj.url = `https://bl.ocks.org${url_end}`;
 
         console.log(`Got block number ${obj.block_number}\t${obj.headline}`);
 
-				scraperama.html(`https://gist.github.com/${url_end}`, ($) => {
-					
-					obj.img = $("#file-thumbnail-png").find("img").attr("src");
+        scraperama.html(`https://gist.github.com/${url_end}`, ($) => {
+          
+          obj.img = $("#file-thumbnail-png").find("img").attr("src");
 
-					json.push(obj);
+          json.push(obj);
 
-					fs.writeFileSync(`${__dirname}/../data/blocks.json`, JSON.stringify(json));
+          fs.writeFileSync(`${__dirname}/../data/blocks.json`, JSON.stringify(json));
 
-				});
+        });
 
-			}
-			
-		});
+      }
+      
+    });
 
-	});
+  });
 }
 
 
